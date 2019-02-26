a44e9b7924205aa6d3197b8ca30fb019b856de6f
mrsalvini <dsgmcosta@gmail.com>
2019-02-26 16:48:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+
+  new_hash = {}
+
+  # first step adds the languages level and adds type
+  languages.each { |style, lang_hash|
+
+    lang_hash.each { |lang, type_hash|
+      if new_hash.member?(lang)
+        new_hash[lang][:style] << style
+      else
+        new_hash[lang] = {
+          type: ""
+        }
+
+        new_hash[lang][:style] = [style]
+
+        type_hash.each { |type, type_string|
+          new_hash[lang][type] = type_string
+        }
+      end
+    }
+  }
+
+  # now we do the type spearately because it has more intricacies
+
+
+  new_hash
+
 end
